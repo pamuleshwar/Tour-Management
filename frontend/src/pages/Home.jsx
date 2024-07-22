a20cc8e93ea4e5b8c01229a8c9519e870088a368
pamuleshwar <pamuleshwar@gmail.com>
2024-07-22 11:09:01 +0530

    image gallery

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,12 +6,14 @@ import {Container, Row, Col} from 'reactstrap';
 import heroImg from '../assets/images/hero-img01.jpg';
 import heroImg02 from '../assets/images/hero-img02.jpg';
 import heroVideo from '../assets/images/hero-video.mp4';
-import worldImg from '../assets/images/world.png'
+import worldImg from '../assets/images/world.png';
+import experienceImg from '../assets/images/experience.png';
 
 import Subtitle from '../shared/Subtitle';
 import SearchBar from '../shared/SearchBar';
 import ServiceList from '../services/ServiceList';
 import FeaturedTourList from '../components/Featured-tours/FeaturedTourList';
+import MasonryImagesGallery from '../components/Image-gallery/MasonryImagesGallery';
 
 const Home = () => {
   return <>
@@ -77,6 +79,67 @@ const Home = () => {
     </Container>
   </section>
   {/* ==================featured tour seciont end ==================== */}
+
+  {/* ================ experience section start ========================= */}
+  <section>
+    <Container>
+      <Row>
+        <Col lg='6'>
+          <div className="experience_content">
+            <Subtitle subtitle={"Experience"} />
+
+            <h2>
+              With our all experience <br /> we will serve you
+            </h2>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br />Neque doloremque excepturi rerum delectus accusamus porro, ullam ipsa? Commodi repellendus officiis dolor.</p>
+          </div>
+
+          <div className="counter_wrapper d-flex align-items-center gap-5">
+            <div className="counter_box">
+              <span>12k+</span>
+              <h6>Successful Trip</h6>
+            </div>
+
+            <div className="counter_box">
+              <span>2k+</span>
+              <h6>Regular</h6>
+            </div>
+
+            <div className="counter_box">
+              <span>15</span>
+              <h6>Years</h6>
+            </div>
+          </div>
+        </Col>
+
+        <Col lg='6'>
+          <div className="experience_img">
+            <img src={experienceImg} alt="experience img" />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  </section>
+  {/* ================ experience section end ========================= */}
+
+  {/* ========= gallery section start ============= */}
+  <section>
+    <Container>
+      <Row>
+        <Col lg='12'>
+          <Subtitle subtitle={"Gallery"} />
+          <h2 className="gallery_title">
+            Visit our customers tour gallery
+          </h2>
+        </Col>
+        <Col lg='12'>
+        <MasonryImagesGallery />
+        </Col>
+      </Row>
+    </Container>
+  </section>
+  {/* ========= gallery section end ============= */}
   </>
 }
 
